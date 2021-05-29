2bc0dc9ed0f036c6ade487703ecd088d94edd4fb
chrisajohnson <johnsonc517@gmail.com>
2021-05-28 17:57:46 -0700

    Can now input data on C. tomentosicollis

--- Habitat temperatures.R
+++ Habitat temperatures.R
@@ -11,12 +11,13 @@ library(tidyverse)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # Read data
-read.data <- read_csv("Habitat Temperatures.csv")
+read.data <- read_csv("Habitat temperatures.csv")
 temp.data <- as.data.frame(read.data)
 
 # Select an insect by removing # in front of name and placing # in front of other species
-sp.data <- subset(temp.data, Species == "Clavigralla shadabi")
+#sp.data <- subset(temp.data, Species == "Clavigralla shadabi")
 #sp.data <- subset(temp.data, Species == "Clavigralla tomentosicollis Benin")
+sp.data <- subset(temp.data, Species == "Clavigralla tomentosicollis Nigeria A")
 
 # Remove columns that do not contain temperature data
 sp.data <- sp.data[-1:-7]
