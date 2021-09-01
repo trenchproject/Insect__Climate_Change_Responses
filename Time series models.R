395845378a1457af408a075e6fa350ae6e585f3e
chrisajohnson <johnsonc517@gmail.com>
2021-08-31 17:51:45 -0700

    Ran model for Clavigralla shadabi in Benin

--- Time series models.R
+++ Time series models.R
@@ -19,7 +19,7 @@ data <- as.data.frame(read_csv("Temperature response parameters.csv"))
 # Time-series data
 # Select an file by removing # in front of name and placing # in front of other files
 #data.density <- read_csv("Population data Nigeria.csv")
-data.density <- read_csv("Population data China.csv")
+#data.density <- read_csv("Population data China.csv")
 
 # Select time-series data
 #data.TS <- subset(data.density, Plot=="B") # select plot A, B, or C
@@ -39,8 +39,8 @@ data.density <- read_csv("Population data China.csv")
 # READ IN MODEL OUTPUT
 # From DDE population dynamics.py 
 # Select an insect by removing # in front of name and placing # in front of other species
-#data.model <- as.data.frame(read_csv("Time Series Clavigralla shadabi Benin.csv"))
-#sp.data <- subset(data, Species == "Clavigralla shadabi Benin")
+data.model <- as.data.frame(read_csv("Time Series Clavigralla shadabi Benin.csv"))
+sp.data <- subset(data, Species == "Clavigralla shadabi Benin")
 #data.model <- as.data.frame(read_csv("Time Series Clavigralla tomentosicollis Benin.csv"))
 #sp.data <- subset(data, Species == "Clavigralla tomentosicollis Benin")
 #data.model <- as.data.frame(read_csv("Time Series Clavigralla tomentosicollis Nigeria B.csv"))
@@ -67,8 +67,8 @@ data.density <- read_csv("Population data China.csv")
 #sp.data <- subset(data, Species == "Pilophorus typicus Japan")
 #data.model <- as.data.frame(read_csv("Time Series Macrolophus pygmaeus on Myzus persicae Greece.csv"))
 #sp.data <- subset(data, Species == "Macrolophus pygmaeus on Myzus persicae Greece")
-data.model <- as.data.frame(read_csv("Time Series Macrolophus pygmaeus on Trialeurodes vaporariorum Greece.csv"))
-sp.data <- subset(data, Species == "Macrolophus pygmaeus on Trialeurodes vaporariorum Greece")
+#data.model <- as.data.frame(read_csv("Time Series Macrolophus pygmaeus on Trialeurodes vaporariorum Greece.csv"))
+#sp.data <- subset(data, Species == "Macrolophus pygmaeus on Trialeurodes vaporariorum Greece")
 
 
 # Population dynamics with climate change
@@ -77,16 +77,19 @@ data.model.CC <- data.model
 
 # SET PLOT OPTIONS
 # Default: plot last 2 year of model
-# For historical time period
-xmin <- 0 #200
-xmax <- 720 #750
+# for historical time period
+xmin <- 0
+xmax <- 720
 ymin <- 0
-ymax <- 500
-# For climate change time period
-xmin.CC <- 0 #200
-xmax.CC <- 720 #750
+ymax <- 200
+# for climate change time period
+xmin.CC <- 0
+xmax.CC <- 720
 ymin.CC <- 0
-ymax.CC <- 500
+ymax.CC <- 200
+# for temperature function
+temp.min <- 295
+temp.max <- 310
 yr <- 360 # days in a year (using 360 for simplicity)
 init_yrs <- 10 # number of years to initiate the model (from Python DDE model)
 TS.length <- xmax - xmin # length of time-series data
@@ -95,9 +98,9 @@ end <- nrow(data.model)
 
 # FORMAT MODEL OUTPUT TO ALIGN WITH TIME-SERIES DATA
 # Remove all rows before time-series data starts
-# for Clavigralla tomentosicollis
+# for Clavigralla tomentosicollis in Nigeria
 #data.model <- data.model[c(-1:-((1972-1961+init_yrs)*yr + xmin)), ]
-# for Apolygus lucorum
+# for Apolygus lucorum in China
 #data.model <- data.model[c(-1:-((2005-1961+init_yrs)*yr + xmin)), ]
 # for other species
 data.model <- data.model[c(-1:-((2020-1961+init_yrs)*yr + xmin)), ]
@@ -233,11 +236,11 @@ model.I.CC = ggplot(data.model.CC, aes(x=Time, y=J+A)) +
 
 
 # PLOT HABITAT TEMPERATURE FUNCTION
-temp.min <- 280
-temp.max <- 310
 # Historical time period
 plot.temp <- ggplot() +
-  geom_function(fun = function(t) (sp.data$meanT + sp.data$delta_mean*(t+time.shift))  + (sp.data$amplT + sp.data$delta_ampl*(t+time.shift)) * sin(2*pi*((t+time.shift) + sp.data$shiftT)/yr),
+  #geom_function(fun = function(t) (sp.data$meanT + sp.data$delta_mean*(t+time.shift))  + (sp.data$amplT + sp.data$delta_ampl*(t+time.shift)) * sin(2*pi*((t+time.shift) + sp.data$shiftT)/yr),
+  #              size=0.8, color="red") +
+  geom_function(fun = function(t) (299.08 + 0.000127*(t+time.shift) + 1.84*cos(2*pi*30/(30*yr)*((t+time.shift)-15) - 1.86) + 1.51*cos(2*pi*60/(30*yr)*((t+time.shift)-15) - 3.09) + 0.359*cos(2*pi*90/(30*yr)*((t+time.shift)-15) - 2.57)),
                 size=0.8, color="red") +
   labs(x="Time", y="T (K)") +
   scale_x_continuous(limits=c(xmin, xmax)) +
@@ -249,7 +252,9 @@ plot.temp <- ggplot() +
 
 # climate change time period
 plot.temp.CC <- ggplot() +
-  geom_function(fun = function(t) (sp.data$meanT + sp.data$delta_mean*(t+time.shift.CC))  + (sp.data$amplT + sp.data$delta_ampl*(t+time.shift.CC)) * sin(2*pi*((t+time.shift.CC) + sp.data$shiftT)/yr),
+  #geom_function(fun = function(t) (sp.data$meanT + sp.data$delta_mean*(t+time.shift.CC))  + (sp.data$amplT + sp.data$delta_ampl*(t+time.shift.CC)) * sin(2*pi*((t+time.shift.CC) + sp.data$shiftT)/yr),
+  #              size=0.8, linetype="longdash", color="red") +
+  geom_function(fun = function(t) (299.08 + 0.000127*(t+time.shift.CC) + 1.84*cos(2*pi*30/(30*yr)*((t+time.shift.CC)-15) - 1.86) + 1.51*cos(2*pi*60/(30*yr)*((t+time.shift.CC)-15) - 3.09) + 0.359*cos(2*pi*90/(30*yr)*((t+time.shift.CC)-15) - 2.57)),
                 size=0.8, linetype="longdash", color="red") +
   labs(x="Time", y="T (K)") +
   scale_x_continuous(limits=c(xmin, xmax)) +
@@ -266,18 +271,18 @@ plot.temp.CC <- ggplot() +
 plot <- ggdraw()  +
   draw_plot(plot.temp, x = 0, y = 0, width = 1, height = 0.3) +
   #draw_plot(plot.J, x = 0, y = 0.3, width = 1, height = 0.7) +
-  #draw_plot(plot.A, x = 0, y = 0.3, width = 1, height = 0.7) +
-  draw_plot(model.J, x = 0, y = 0.3, width = 1, height = 0.7) +
+  draw_plot(plot.A, x = 0, y = 0.3, width = 1, height = 0.7) +
+  #draw_plot(model.J, x = 0, y = 0.3, width = 1, height = 0.7) +
   draw_plot(model.A, x = 0, y = 0.3, width = 1, height = 0.7)
-#plot
+plot
 
 # Total insects
-plot <- ggdraw()  +
-  draw_plot(plot.temp, x = 0, y = 0, width = 1, height = 0.3) +
-  draw_plot(plot.A, x = 0, y = 0.3, width = 1, height = 0.7) +
-  draw_plot(model.J, x = 0, y = 0.3, width = 1, height = 0.7) +
-  draw_plot(model.A, x = 0, y = 0.3, width = 1, height = 0.7) +
-  draw_plot(model.I, x = 0, y = 0.3, width = 1, height = 0.7)
+#plot <- ggdraw()  +
+#  draw_plot(plot.temp, x = 0, y = 0, width = 1, height = 0.3) +
+#  draw_plot(plot.A, x = 0, y = 0.3, width = 1, height = 0.7) +
+#  draw_plot(model.J, x = 0, y = 0.3, width = 1, height = 0.7) +
+#  draw_plot(model.A, x = 0, y = 0.3, width = 1, height = 0.7) +
+#  draw_plot(model.I, x = 0, y = 0.3, width = 1, height = 0.7)
 #plot
 
 # Climate change time period
@@ -289,11 +294,11 @@ plot.CC <- ggdraw()  +
 #plot.CC
 
 # Total insects
-plot.CC <- ggdraw()  +
-  draw_plot(plot.temp.CC, x = 0, y = 0, width = 1, height = 0.3) +
-  draw_plot(model.J.CC, x = 0, y = 0.3, width = 1, height = 0.7) +
-  draw_plot(model.A.CC, x = 0, y = 0.3, width = 1, height = 0.7) +
-  draw_plot(model.I.CC, x = 0, y = 0.3, width = 1, height = 0.7)
+#plot.CC <- ggdraw()  +
+#  draw_plot(plot.temp.CC, x = 0, y = 0, width = 1, height = 0.3) +
+#  draw_plot(model.J.CC, x = 0, y = 0.3, width = 1, height = 0.7) +
+#  draw_plot(model.A.CC, x = 0, y = 0.3, width = 1, height = 0.7) +
+#  draw_plot(model.I.CC, x = 0, y = 0.3, width = 1, height = 0.7)
 #plot.CC
 
 # Compare historical and climate change periods
@@ -317,7 +322,9 @@ plot.compare
 
 
 
-# CALCULATE DENSITY METRICS
+
+
+# CALCULATE CHANGES BETWEEN HISTORICAL AND CLIMATE CHANGE PERIODS
 # mean density
 mean.J <- mean(data.model[["J"]])
 mean.J.CC <- mean(data.model.CC[["J"]])
@@ -339,8 +346,8 @@ time.J <- subset(data.model, J==max.J)$Time%%yr
 time.J.CC <- subset(data.model.CC, J==max.J.CC)$Time%%yr
 time.A <- subset(data.model, A==max.A)$Time%%yr
 time.A.CC <- subset(data.model.CC, A==max.A.CC)$Time%%yr
-d.time.J <- (time.J.CC-time.J)/time.J
-d.time.A <- (time.A.CC-time.A)/time.A
+d.time.J <- (time.J.CC-time.J)
+d.time.A <- (time.A.CC-time.A)
 
 # minimum density
 min.J <- round(min(data.model[["J"]]), digits=2)
@@ -363,41 +370,50 @@ for(i in 0:nrow(data.model.CC)) {
   if(sp.data["bTopt"]*exp(-((temp.CC(i)-sp.data["Toptb"])^2)/(2*sp.data["sb"]^2)) > 0.1*sp.data["bTopt"]) {b.period.CC <- b.period.CC + 1} }
 d.b <- (b.period.CC-b.period)/b.period
 
-# cumulative reproductive (sum b(T))
-b.sum <- 0
-b.sum.CC <- 0
-for(i in 0:nrow(data.model)) {
-  b.sum <- b.sum + (sp.data["bTopt"]*exp(-((temp(i)-sp.data["Toptb"])^2)/(2*sp.data["sb"]^2)))[[1]] }
-for(i in 0:nrow(data.model.CC)) {
-  b.sum.CC <- b.sum.CC + (sp.data["bTopt"]*exp(-((temp.CC(i)-sp.data["Toptb"])^2)/(2*sp.data["sb"]^2)))[[1]] }
-d.b.sum <- (b.sum.CC-b.sum)/b.sum
-
 # developmental activity period (development rate > 0.25 Mmax or > dMin from Jahansson et al. 2020)
 # calculate Mmax and Topt
 Mmax <- 0
 Topt <- 0
 for(i in 0:100) {
   T <- coef(r.T)[1] + i*(sp.data["TH"]-sp.data["TL"])/100
-  M.T <- sp.data["mTR"]*(T/sp.data["TR"])*exp(sp.data["AmJ"]*(1/sp.data["TR"]-1/T))/(1+exp(sp.data["AL"]*(1/sp.data["TL"]-1/T))+exp(sp.data["AH"]*(1/sp.data["TH"]-1/T)))
+  M.T <- sp.data["mTR"]*(T/sp.data["TR"])*exp(sp.data["AmJ"]*(1/sp.data["TR"]-1/T))/(1+sp.data["skew"]*exp(sp.data["AL"]*(1/sp.data["TL"]-1/T))+exp(sp.data["AH"]*(1/sp.data["TH"]-1/T)))
   if(M.T > Mmax) {Mmax <- M.T[[1]]
   Topt <- T[[1]]}}
 # calculate time above 0.25 Mmax
 m.period <- 0
 m.period.CC <- 0
 for(i in 0:nrow(data.model)) {
-  if(sp.data["mTR"]*(temp(i)/sp.data["TR"])*exp(sp.data["AmJ"]*(1/sp.data["TR"]-1/temp(i)))/(1+exp(sp.data["AL"]*(1/sp.data["TL"]-1/temp(i)))+exp(sp.data["AH"]*(1/sp.data["TH"]-1/temp(i)))) > 0.25*Mmax) {m.period = m.period + 1} }
+  if(sp.data["mTR"]*(temp(i)/sp.data["TR"])*exp(sp.data["AmJ"]*(1/sp.data["TR"]-1/temp(i)))/(1+sp.data["skew"]*exp(sp.data["AL"]*(1/sp.data["TL"]-1/temp(i)))+exp(sp.data["AH"]*(1/sp.data["TH"]-1/temp(i)))) > 0.25*Mmax) {m.period = m.period + 1} }
 for(i in 0:nrow(data.model.CC)) {
-  if(sp.data["mTR"]*(temp.CC(i)/sp.data["TR"])*exp(sp.data["AmJ"]*(1/sp.data["TR"]-1/temp.CC(i)))/(1+exp(sp.data["AL"]*(1/sp.data["TL"]-1/temp.CC(i)))+exp(sp.data["AH"]*(1/sp.data["TH"]-1/temp.CC(i)))) > 0.25*Mmax) {m.period.CC = m.period.CC + 1} }
+  if(sp.data["mTR"]*(temp.CC(i)/sp.data["TR"])*exp(sp.data["AmJ"]*(1/sp.data["TR"]-1/temp.CC(i)))/(1+sp.data["skew"]*exp(sp.data["AL"]*(1/sp.data["TL"]-1/temp.CC(i)))+exp(sp.data["AH"]*(1/sp.data["TH"]-1/temp.CC(i)))) > 0.25*Mmax) {m.period.CC = m.period.CC + 1} }
 d.m <- (m.period.CC-m.period)/m.period
 
-# cumulative adult mortality (sum dA(T))
+# average reproductive rate (mean b(T))
+b.sum <- 0
+b.sum.CC <- 0
+for(i in 0:nrow(data.model)) {
+  b.sum <- b.sum + (sp.data["bTopt"]*exp(-((temp(i)-sp.data["Toptb"])^2)/(2*sp.data["sb"]^2)))[[1]] }
+for(i in 0:nrow(data.model.CC)) {
+  b.sum.CC <- b.sum.CC + (sp.data["bTopt"]*exp(-((temp.CC(i)-sp.data["Toptb"])^2)/(2*sp.data["sb"]^2)))[[1]] }
+d.b.ave <- (b.sum.CC-b.sum)/b.sum
+
+# average development rate (mean m(T))
+m.sum <- 0
+m.sum.CC <- 0
+for(i in 0:nrow(data.model)) {
+  m.sum <- m.sum + (sp.data["mTR"]*(temp(i)/sp.data["TR"])*exp(sp.data["AmJ"]*(1/sp.data["TR"]-1/temp(i)))/(1+sp.data["skew"]*exp(sp.data["AL"]*(1/sp.data["TL"]-1/temp(i)))+exp(sp.data["AH"]*(1/sp.data["TH"]-1/temp(i)))))[[1]] }
+for(i in 0:nrow(data.model.CC)) {
+  m.sum.CC <- m.sum.CC + (sp.data["mTR"]*(temp.CC(i)/sp.data["TR"])*exp(sp.data["AmJ"]*(1/sp.data["TR"]-1/temp.CC(i)))/(1+sp.data["skew"]*exp(sp.data["AL"]*(1/sp.data["TL"]-1/temp.CC(i)))+exp(sp.data["AH"]*(1/sp.data["TH"]-1/temp.CC(i)))))[[1]] }
+d.m.ave <- (m.sum.CC-m.sum)/m.sum
+
+# average adult mortality rate (mean dA(T))
 dA.sum <- 0
 dA.sum.CC <- 0
 for(i in 0:nrow(data.model)) {
   dA.sum <- dA.sum + (sp.data["dATR"]*exp(sp.data["AdA"]*(1/sp.data["TR"]-1/temp(i))))[[1]] }
 for(i in 0:nrow(data.model.CC)) {
   dA.sum.CC <- dA.sum.CC + (sp.data["dATR"]*exp(sp.data["AdA"]*(1/sp.data["TR"]-1/temp.CC(i))))[[1]] }
-d.dA.sum <- (dA.sum.CC-dA.sum)/dA.sum
+d.dA.ave <- (dA.sum.CC-dA.sum)/dA.sum
 
 # time above optimum range for reproductive rate (T > ToptR0 + sR0)
 R0.period <- 0
@@ -406,7 +422,7 @@ for(i in 0:nrow(data.model)) {
   if(temp(i) > sp.data["ToptR0"] + sp.data["sR0"]) {R0.period = R0.period + 1} }
 for(i in 0:nrow(data.model.CC)) {
   if(temp.CC(i) > sp.data["ToptR0"] + sp.data["sR0"]) {R0.period.CC <- R0.period.CC + 1} }
-d.R0 <- (R0.period.CC-R0.period)/R0.period
+if(R0.period !=0) { d.R0 <- (R0.period.CC-R0.period)/R0.period } else {d.R0 <- 0 }
 
 # time above rTmax (T > rTmax)
 r.period <- 0
@@ -415,29 +431,39 @@ for(i in 0:nrow(data.model)) {
   if(temp(i) > sp.data["rTmax"]) {r.period = r.period + 1} }
 for(i in 0:nrow(data.model.CC)) {
   if(temp.CC(i) > sp.data["rTmax"]) {r.period.CC <- r.period.CC + 1} }
-d.r <- (r.period.CC-r.period)/r.period
+if(r.period !=0) { d.r <- (r.period.CC-r.period)/r.period } else {d.r <- 0 }
 
 # mean thermal safety margin (Toptr - T)
 TSM <- 0
 TSM.CC <- 0
 for(i in 0:nrow(data.model)) {
-  TSM <- TSM + (sp.data["Toptr"][[1]] - temp(i)) }
+  if(temp(i) > sp.data["Toptr"]) {TSM = TSM + 1} }
 for(i in 0:nrow(data.model.CC)) {
-  TSM.CC <- TSM.CC + (sp.data["Toptr"][[1]] - temp.CC(i)) }
-d.TSM <- (TSM.CC-TSM)/TSM
+  if(temp.CC(i) > sp.data["Toptr"]) {TSM.CC <- TSM.CC + 1} }
+if(TSM !=0) { d.TSM <- (TSM.CC-TSM)/TSM } else {d.TSM <- 0 }
 
 
-# PLOT DENSITY METRICS
-par(mfrow=c(2,4))
-barplot(c(d.mean.J,d.mean.A), col=c("#d1495b","#30638e"), ylim=c(-0.5,0.5), main=expression("Mean density"))
-barplot(c(d.max.J,d.max.A), col=c("#d1495b","#30638e"), ylim=c(-0.5,0.5), main=expression("Peak density"))
-barplot(c(d.time.J,d.time.A), col=c("#d1495b","#30638e"), ylim=c(-0.5,0.5), main=expression("Timing of peak"))
-#barplot(c(d.min.J,d.min.A), col=c("#d1495b","#30638e"), ylim=c(-0.5,0.5), main=expression("Minimum density"))
+
+# PLOTS
+par(mfrow=c(3,5))
+# density metrics
+barplot(c(d.mean.J,d.mean.A), col=c("#d1495b","#30638e"), ylim=c(-0.5,2), main=expression("Mean density"))
+barplot(c(d.max.J,d.max.A), col=c("#d1495b","#30638e"), ylim=c(-0.5,4), main=expression("Peak density"))
+barplot(c(d.time.J,d.time.A), col=c("#d1495b","#30638e"), ylim=c(-200,200), main=expression("Timing of peak"))
+barplot(c(d.min.J,d.min.A), col=c("#d1495b","#30638e"), ylim=c(0,10), main=expression("Minimum density"))
+# activity periods
 barplot(d.b, col="#30638e", xlim=c(0.2,2), ylim=c(-0.5,0.5), main=expression("Rep. period"))
-#barplot(d.b.sum, col="#30638e", xlim=c(0.2,2), ylim=c(-0.5,0.5), main=expression("Sum rep."))
 barplot(d.m, col="#d1495b", xlim=c(0.2,2), ylim=c(-0.5,0.5), main=expression("Dev. period"))
-barplot(d.dA.sum, col="#30638e", xlim=c(0.2,2), ylim=c(-0.5,0.5), main=expression("Adult mortality"))
+# average life history traits
+barplot(d.b.ave, col="#30638e", xlim=c(0.2,2), ylim=c(-0.5,0.5), main=expression("Mean b(T)"))
+barplot(d.m.ave, col="#d1495b", xlim=c(0.2,2), ylim=c(-0.5,0.5), main=expression("Mean m(T)"))
+barplot(d.dA.ave, col="#30638e", xlim=c(0.2,2), ylim=c(-0.5,2.5), main=expression("Mean dA(T)"))
+# thermal performance curves
 barplot(d.R0, col="#30638e", xlim=c(0.2,2), ylim=c(-2,2), main=expression("above R0"))
-#barplot(d.r, col="#30638e", xlim=c(0.2,2), ylim=c(-2,2), main=expression("above R0"))
+barplot(d.r, col="#30638e", xlim=c(0.2,2), ylim=c(-2,2), main=expression("above r"))
 barplot(d.TSM, col="#30638e", xlim=c(0.2,2), ylim=c(-0.5,0.5), main=expression("Thermal margin"))
+# habitat temperatures
+barplot((sp.data["ext_meanT"])[[1]], col="#30638e", xlim=c(0.2,2), ylim=c(0,10), main=expression("meanT"))
+barplot((sp.data["ext_amplT"])[[1]], col="#30638e", xlim=c(0.2,2), ylim=c(0,10), main=expression("amplT"))
+barplot((sp.data["ext_mT_aT"])[[1]], col="#30638e", xlim=c(0.2,2), ylim=c(0,10), main=expression("meanT + amplT"))
 par(mfrow=c(1,1))
