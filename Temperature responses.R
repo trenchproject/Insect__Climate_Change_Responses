e3702bd4b35ce428b8966a88b19be1dbca8c3430
chrisajohnson <johnsonc517@gmail.com>
2021-12-03 06:57:17 -0800

    Have now fit life history trait responses for 30 insects

--- Temperature responses.R
+++ Temperature responses.R
@@ -14,7 +14,7 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 data <- as.data.frame(read_csv("Temperature response data.csv"))
 
 # USER: enter species name (used in temperature response data.csv)
-name <- "Hyperomyzus lactucae"
+name <- "Hyadaphis pseudobrassicae"
 
 # Assign species
 sp.data <- data[data$Species == name,]
@@ -43,7 +43,7 @@ points(seq(Tmin,Tmax,1),coef(fec)[1]*exp(-((seq(Tmin,Tmax,1)-coef(fec)[2])^2)/(2
 ###################################### DEVELOPMENT ##########################################
 # estimate xTR and A
 # NOTE: removed data beyond max development
-dev.mon <- nls(Development ~ xTR*T_K/TR*exp(A*(1/TR-1/T_K)), data=sp.data[-c((nrow(sp.data)-0):nrow(sp.data)),],
+dev.mon <- nls(Development ~ xTR*T_K/TR*exp(A*(1/TR-1/T_K)), data=sp.data[-c((nrow(sp.data)-1):nrow(sp.data)),],
                start=list(xTR=0.1, A=1000))
 summary(dev.mon)
 # Plot model fits
@@ -54,9 +54,9 @@ points(seq(Tmin,Tmax,1), coef(dev.mon)[1]*(seq(Tmin,Tmax,1)/TR)*exp(coef(dev.mon
 xTR <- coef(dev.mon)[1]
 A <- coef(dev.mon)[2]
 kTL <- 280
-kTH <- 300
+kTH <- 303
 dev.A <- nls(Development ~ xTR*(T_K/TR)*exp(A*(1/TR-1/T_K))/(1+exp(AL*(1/kTL-1/T_K))+exp(AH*(1/kTH-1/T_K))),
-              data=sp.data, start=list(AL=-50000, AH=50000))
+              data=sp.data, start=list(AL=-50000, AH=100000))
 summary(dev.A)
 AL <- coef(dev.A)[1]
 AH <- coef(dev.A)[2]
@@ -71,24 +71,26 @@ points(seq(Tmin,Tmax,1), xTR*(seq(Tmin,Tmax,1)/TR)*exp(A*(1/TR-1/seq(Tmin,Tmax,1
 
 # estimate TH and AH separately from TL and AL if needed
 kAL <- -50000
-dev.H <- nls(Development ~ coef(dev.mon)[1]*(T_K/TR)*exp(coef(dev.mon)[2]*(1/TR-1/T_K))/(1+exp(kAL*(1/kTL-1/T_K))+exp(AH*(1/TH-1/T_K))),
+dev.H <- nls(Development ~ xTR*(T_K/TR)*exp(A*(1/TR-1/T_K))/(1+exp(kAL*(1/kTL-1/T_K))+exp(AH*(1/TH-1/T_K))),
              data=sp.data, start=list(TH=kTH, AH=10000))
 summary(dev.H)
-dev.AL <- nls(Development ~ coef(dev.mon)[1]*(T_K/TR)*exp(coef(dev.mon)[2]*(1/TR-1/T_K))/(1+exp(AL*(1/kTL-1/T_K))+exp(coef(dev.H)[2]*(1/coef(dev.H)[1]-1/T_K))),
-             data=sp.data, start=list(AL=-100000))
+kTH <- coef(dev.H)[1]
+kAH <- coef(dev.H)[2]
+dev.AL <- nls(Development ~ xTR*(T_K/TR)*exp(A*(1/TR-1/T_K))/(1+exp(AL*(1/kTL-1/T_K))+exp(kAH*(1/kTH-1/T_K))),
+             data=sp.data, start=list(AL=-10000))
 summary(dev.AL)
-dev.TL <- nls(Development ~ coef(dev.mon)[1]*(T_K/TR)*exp(coef(dev.mon)[2]*(1/TR-1/T_K))/(1+exp(coef(dev.AL)[1]*(1/TL-1/T_K))+exp(coef(dev.H)[2]*(1/coef(dev.H)[1]-1/T_K))),
+dev.TL <- nls(Development ~ xTR*(T_K/TR)*exp(A*(1/TR-1/T_K))/(1+exp(coef(dev.AL)[1]*(1/TL-1/T_K))+exp(coef(dev.H)[2]*(1/coef(dev.H)[1]-1/T_K))),
              data=sp.data, start=list(TL=kTL))
 summary(dev.TL)
 # Plot model fits
 plot(sp.data$T_K, sp.data$Development)
-points(seq(Tmin,Tmax,1), coef(dev.mon)[1]*(seq(Tmin,Tmax,1)/TR)*exp(coef(dev.mon)[2]*(1/TR-1/seq(Tmin,Tmax,1)))/
+points(seq(Tmin,Tmax,1), xTR*(seq(Tmin,Tmax,1)/TR)*exp(A*(1/TR-1/seq(Tmin,Tmax,1)))/
          (1+(exp(coef(dev.AL)[1]*(1/coef(dev.TL)[1]-1/seq(Tmin,Tmax,1)))+exp(coef(dev.H)[2]*(1/coef(dev.H)[1]-1/seq(Tmin,Tmax,1))))), type="l", col="blue")
 
 
 # Minimum developmental temperature
 # NOTE: removed data beyond max development
-dev.min <- nls(Development ~ m*T_K+b, data=sp.data[-c((nrow(sp.data)-0):nrow(sp.data)),],
+dev.min <- nls(Development ~ m*T_K+b, data=sp.data[-c((nrow(sp.data)-1):nrow(sp.data)),],
                start=list(m=0.01, b=0))
 summary(dev.min)
 # Plot model fits
@@ -125,21 +127,21 @@ points(seq(Tmin,Tmax,1), coef(mort.A)[1]*exp(coef(mort.A)[2]*(1/TR-1/seq(Tmin,Tm
 # estimate all parameters
 r <- nls(r ~ ifelse(T_K <= Topt, rMax*exp(-1*((T_K-Topt)/(2*sr))^2),
                     rMax*(1 - ((T_K-Topt)/(Topt-Tmax))^2)),
-                    data=sp.data, start=list(sr=5, Topt=295, Tmax=300, rMax=1.5))
+                    data=sp.data, start=list(sr=5, Topt=295, Tmax=300, rMax=0.3))
 summary(r)
 # Plot model fits
-plot(sp.data$T_K, sp.data$r, xlim = c(Tmin,Tmax+5), ylim = c(0,max(sp.data$r)+0.05))
+plot(sp.data$T_K, sp.data$r, xlim = c(Tmin,Tmax+5), ylim = c(0,0.5)) #max(sp.data$r)+0.05))
 points(seq(Tmin,Tmax+5,1), ifelse(seq(Tmin,Tmax+5,1) <= coef(r)[2], coef(r)[4]*exp(-1*((seq(Tmin,Tmax+5,1)-coef(r)[2])/(2*coef(r)[1]))^2),
                                 coef(r)[4]*(1 - ((seq(Tmin,Tmax+5,1)-coef(r)[2])/(coef(r)[2]-coef(r)[3]))^2)), type="l", col="blue")
 
 
 # set Topt and rMax (NOTE: Topt cannot equal Tmax in nls)
-Topt <- sp.data[nrow(sp.data)-1,"T_K"]
+Topt <- sp.data[nrow(sp.data)-2,"T_K"]
 rMax <- sp.data[sp.data$T_K==Topt,"r"]
 # estimate all other parameters
 r <- nls(r ~ ifelse(T_K <= Topt, rMax*exp(-1*((T_K-Topt)/(2*sr))^2),
                     rMax*(1 - ((T_K-Topt)/(Topt-Tmax))^2)),
-         data=sp.data, start=list(sr=3, Tmax=303))
+         data=sp.data, start=list(sr=3, Tmax=298))
 summary(r)
 # Plot model fits
 plot(sp.data$T_K, sp.data$r, xlim=c(Tmin,Tmax), ylim=c(0,rMax))
