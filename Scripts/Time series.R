592058605b8ec05f1e46d678939945158569b8a6
Chris Johnson <johnsonc517@gmail.com>
2023-05-04 16:01:17 +0200

    Updated "Time series.R" and wrote ReadMe for it

--- Scripts/Time series.R
+++ Scripts/Time series.R
@@ -1,6 +1,6 @@
-########################################################################
-#### This R script fits model outputs to empirical time-series data ####
-########################################################################
+#################################################################################
+## This R script plots model population dynamics (including field census data) ##
+#################################################################################
 
 # Load packages and set working directory
 library(tidyverse)
@@ -14,13 +14,12 @@ library(stringr) # for manipulating strings
 
 # NOTE: CLEAR GLOBAL ENVIRONMENT IF ELAPSED TIME LIMIT ERROR OCCURS OR LINES ARE MISSING FROM PLOTS
 
-# USER: enter species and location
-species <- "Apolygus lucorum"
-location <- "China Dafeng"
-#species <- "Clavigralla shadabi"
-#location <- "Benin"
 
-# USER: Fit model prediction to field census data?
+# USER: enter species and location
+species <- "Clavigralla shadabi"
+location <- "Benin"
+
+# USER: Plot model population dynamics with field census data?
 census <- TRUE
 
 
@@ -54,12 +53,12 @@ start.f <- 65 # how many years into climate change period to start population dy
 xmin <- 0 # always plot model starting at time zero
 xmax <- 2*yr # plot 2 years of population dynamics
 ymin <- 0 # min density for plots
-ymax <- 30 # max density for plots
+ymax <- 125 # max density for plots
 temp.min <- 0 # min temperature in C for plots
 temp.max <- 40 # max temperature in C for plots
 
 
-# FORMAT MODEL OUTPUT TO ALIGN WITH TIME-SERIES DATA
+# FORMAT MODEL OUTPUT TO ALIGN WITH CENSUS DATA
 # Extract time-series data for plotting
 model.r <- model.r[(nrow(model.r) - 2*yr + 1):nrow(model.r),]
 model.f <- model.f[(nrow(model.f) - 2*yr + 1):nrow(model.f),]
@@ -96,75 +95,80 @@ temp.f <- ggplot() +
         axis.text = element_text(size=13), axis.title = element_text(size=1.50))
 
 
-# PLOT TEMPERATURE BANDS AROUND WARMEST HALF OF YEAR
-# Tropical habitats
-# for temperature plots
-band.temp <- ggplot() +
-  geom_rect(aes(xmin=xmin, xmax=xmin + 3*yr/4 - t.param$shiftT.r, ymin=0, ymax=Inf), fill="grey", alpha=0.5) +
-  geom_rect(aes(xmin=xmin + 5*yr/4 - t.param$shiftT.r, xmax=xmin + 7*yr/4 - t.param$shiftT.r, ymin=0, ymax=Inf), fill="grey", alpha=0.5) +
-  geom_rect(aes(xmin=xmin + 9*yr/4 - t.param$shiftT.r, xmax=xmax, ymin=0, ymax=Inf), fill="grey", alpha=0.5) +
-  labs(x="", y="") +
-  scale_x_continuous(limits=c(xmin, xmax)) +
-  scale_y_continuous(limits=c(temp.min, temp.max)) +
-  #scale_y_log10(limits=c(ymin, ymax)) +
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-        panel.background = element_rect(fill="transparent"), plot.background = element_rect(fill="transparent"),
-        axis.line = element_line(color = "black"), legend.position = "none", 
-        axis.text = element_text(size=13), axis.title = element_text(size=1.50))
+# PLOT TEMPERATURE BANDS AROUND THE WARMEST HALF OF YEAR
+# Tropical/subtropical habitats
+if(t.param$Habitat == "Tropical" || t.param$Habitat == "Subropical") {
+  # for habitat temperature plots
+  band.temp <- ggplot() +
+    geom_rect(aes(xmin=xmin, xmax=xmin + 3*yr/4 - t.param$shiftT.r, ymin=0, ymax=Inf), fill="grey", alpha=0.5) +
+    geom_rect(aes(xmin=xmin + 5*yr/4 - t.param$shiftT.r, xmax=xmin + 7*yr/4 - t.param$shiftT.r, ymin=0, ymax=Inf), fill="grey", alpha=0.5) +
+    geom_rect(aes(xmin=xmin + 9*yr/4 - t.param$shiftT.r, xmax=xmax, ymin=0, ymax=Inf), fill="grey", alpha=0.5) +
+    labs(x="", y="") +
+    scale_x_continuous(limits=c(xmin, xmax)) +
+    scale_y_continuous(limits=c(temp.min, temp.max)) +
+    #scale_y_log10(limits=c(ymin, ymax)) +
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+          panel.background = element_rect(fill="transparent"), plot.background = element_rect(fill="transparent"),
+          axis.line = element_line(color = "black"), legend.position = "none", 
+          axis.text = element_text(size=13), axis.title = element_text(size=1.50))
+  
+  # for insect density plots
+  band.density <- ggplot() +
+    geom_rect(aes(xmin=xmin, xmax=xmin + 3*yr/4 - t.param$shiftT.r, ymin=0, ymax=Inf), fill="grey", alpha=0.5) +
+    geom_rect(aes(xmin=xmin + 5*yr/4 - t.param$shiftT.r, xmax=xmin + 7*yr/4 - t.param$shiftT.r, ymin=0, ymax=Inf), fill="grey", alpha=0.5) +
+    geom_rect(aes(xmin=xmin + 9*yr/4 - t.param$shiftT.r, xmax=xmax, ymin=0, ymax=Inf), fill="grey", alpha=0.5) +
+    labs(x="", y="") +
+    scale_x_continuous(limits=c(xmin, xmax)) +
+    scale_y_continuous(limits=c(ymin, ymax)) +
+    #scale_y_log10(limits=c(ymin, ymax)) +
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+          panel.background = element_rect(fill="transparent"), plot.background = element_rect(fill="transparent"),
+          axis.line = element_line(color = "black"), legend.position = "none", 
+          axis.text = element_text(size=13), axis.title = element_text(size=1.50))
 
-# for insect density plots
-band.density <- ggplot() +
-  geom_rect(aes(xmin=xmin, xmax=xmin + 3*yr/4 - t.param$shiftT.r, ymin=0, ymax=Inf), fill="grey", alpha=0.5) +
-  geom_rect(aes(xmin=xmin + 5*yr/4 - t.param$shiftT.r, xmax=xmin + 7*yr/4 - t.param$shiftT.r, ymin=0, ymax=Inf), fill="grey", alpha=0.5) +
-  geom_rect(aes(xmin=xmin + 9*yr/4 - t.param$shiftT.r, xmax=xmax, ymin=0, ymax=Inf), fill="grey", alpha=0.5) +
-  labs(x="", y="") +
-  scale_x_continuous(limits=c(xmin, xmax)) +
-  scale_y_continuous(limits=c(ymin, ymax)) +
-  #scale_y_log10(limits=c(ymin, ymax)) +
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-        panel.background = element_rect(fill="transparent"), plot.background = element_rect(fill="transparent"),
-        axis.line = element_line(color = "black"), legend.position = "none", 
-        axis.text = element_text(size=13), axis.title = element_text(size=1.50))
-
-# Temperate habitats
-# for temperature plots
-band.summer.temp <- ggplot() +
-  geom_rect(aes(xmin=xmin + yr/4, xmax=xmin + 3*yr/4, ymin=0, ymax=Inf), fill="grey", alpha=0.5) +
-  geom_rect(aes(xmin=xmin + 5*yr/4, xmax=xmin + 7*yr/4, ymin=0, ymax=Inf), fill="grey", alpha=0.5) +
-  labs(x="", y="") +
-  scale_x_continuous(limits=c(xmin, xmax)) +
-  scale_y_continuous(limits=c(temp.min, temp.max)) +
-  #scale_y_log10(limits=c(ymin, ymax)) +
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-        panel.background = element_rect(fill="transparent"), plot.background = element_rect(fill="transparent"),
-        axis.line = element_line(color = "black"), legend.position = "none", 
-        axis.text = element_text(size=13), axis.title = element_text(size=1.50))
-
-# for insect density plots
-band.summer.density <- ggplot() +
-  geom_rect(aes(xmin=xmin + yr/4, xmax=xmin + 3*yr/4, ymin=0, ymax=Inf), fill="grey", alpha=0.5) +
-  geom_rect(aes(xmin=xmin + 5*yr/4, xmax=xmin + 7*yr/4, ymin=0, ymax=Inf), fill="grey", alpha=0.5) +
-  labs(x="", y="") +
-  scale_x_continuous(limits=c(xmin, xmax)) +
-  scale_y_continuous(limits=c(ymin, ymax)) +
-  #scale_y_log10(limits=c(ymin, ymax)) +
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-        panel.background = element_rect(fill="transparent"), plot.background = element_rect(fill="transparent"),
-        axis.line = element_line(color = "black"), legend.position = "none", 
-        axis.text = element_text(size=13), axis.title = element_text(size=1.50))
+} else {
+  # Temperate habitats
+  # for habitat temperature plots
+  band.summer.temp <- ggplot() +
+    geom_rect(aes(xmin=xmin + yr/4, xmax=xmin + 3*yr/4, ymin=0, ymax=Inf), fill="grey", alpha=0.5) +
+    geom_rect(aes(xmin=xmin + 5*yr/4, xmax=xmin + 7*yr/4, ymin=0, ymax=Inf), fill="grey", alpha=0.5) +
+    labs(x="", y="") +
+    scale_x_continuous(limits=c(xmin, xmax)) +
+    scale_y_continuous(limits=c(temp.min, temp.max)) +
+    #scale_y_log10(limits=c(ymin, ymax)) +
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+          panel.background = element_rect(fill="transparent"), plot.background = element_rect(fill="transparent"),
+          axis.line = element_line(color = "black"), legend.position = "none", 
+          axis.text = element_text(size=13), axis.title = element_text(size=1.50))
+  
+  # for insect density plots
+  band.summer.density <- ggplot() +
+    geom_rect(aes(xmin=xmin + yr/4, xmax=xmin + 3*yr/4, ymin=0, ymax=Inf), fill="grey", alpha=0.5) +
+    geom_rect(aes(xmin=xmin + 5*yr/4, xmax=xmin + 7*yr/4, ymin=0, ymax=Inf), fill="grey", alpha=0.5) +
+    labs(x="", y="") +
+    scale_x_continuous(limits=c(xmin, xmax)) +
+    scale_y_continuous(limits=c(ymin, ymax)) +
+    #scale_y_log10(limits=c(ymin, ymax)) +
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+          panel.background = element_rect(fill="transparent"), plot.background = element_rect(fill="transparent"),
+          axis.line = element_line(color = "black"), legend.position = "none", 
+          axis.text = element_text(size=13), axis.title = element_text(size=1.50))
+}
 
 
 # FIELD CENSUS DATA
-plot.census <- ggplot(data.census, aes(x=Time, y=A, ymin=A_SE_L, ymax=A_SE_H)) + 
-  geom_pointrange(size=0.5, color="#0072B2") + # blue color
-  labs(x="", y="") +
-  scale_x_continuous(limits=c(xmin, xmax)) +
-  scale_y_continuous(limits=c(ymin, ymax)) +
-  #scale_y_log10(limits=c(ymin, ymax)) +
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-        panel.background = element_rect(fill="transparent"), plot.background = element_rect(fill="transparent"),
-        axis.line = element_line(color = "black"), legend.position = "none", 
-        axis.text = element_text(size=13), axis.title = element_text(size=1.50)) 
+if(census == TRUE) {
+  plot.census <- ggplot(data.census, aes(x=Time, y=A, ymin=A_SE_L, ymax=A_SE_H)) + 
+    geom_pointrange(size=0.5, color="#0072B2") + # blue color
+    labs(x="", y="") +
+    scale_x_continuous(limits=c(xmin, xmax)) +
+    scale_y_continuous(limits=c(ymin, ymax)) +
+    #scale_y_log10(limits=c(ymin, ymax)) +
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+          panel.background = element_rect(fill="transparent"), plot.background = element_rect(fill="transparent"),
+          axis.line = element_line(color = "black"), legend.position = "none", 
+          axis.text = element_text(size=13), axis.title = element_text(size=1.50))
+}
 
 
 # DDE POPULATION MODEL DATA
@@ -221,6 +225,7 @@ model.A.f <- ggplot(model.f, aes(x=Time, y=A)) +
 
 ################################# DRAW PLOTS ####################################
 # Habitat temperature plot (Fig. 1A,B)
+#dev.new()
 if(t.param$Habitat == "Tropical") {
   ggdraw()  +
     draw_plot(band.temp, x = 0, y = 0, width = 1, height = 0.3) +
@@ -233,7 +238,7 @@ if(t.param$Habitat == "Tropical") {
     draw_plot(temp.f, x = 0, y = 0, width = 1, height = 0.3)
 }
 
-# Model and field census data plot (Fig. 1C,D)
+# Model and field census plot (Fig. 1C,D)
 if(census == TRUE) {
   if(t.param$Habitat == "Tropical") {
     ggdraw()  +
@@ -247,95 +252,28 @@ if(census == TRUE) {
       draw_plot(plot.census, x = 0, y = 0, width = 1, height = 0.4) }
 }
 
+# Juvenile density plot (Fig. 1E,F)
+if(t.param$Habitat == "Tropical") {
+  ggdraw()  +
+    draw_plot(band.density, x = 0, y = 0, width = 1, height = 0.4) +
+    draw_plot(model.J.r, x = 0, y = 0, width = 1, height = 0.4) +
+    draw_plot(model.J.f, x = 0, y = 0, width = 1, height = 0.4)
+} else { 
+  ggdraw()  +
+    draw_plot(band.summer.density, x = 0, y = 0, width = 1, height = 0.4) +
+    draw_plot(model.J.r, x = 0, y = 0, width = 1, height = 0.4) +
+    draw_plot(model.J.f, x = 0, y = 0, width = 1, height = 0.4)
+}
 
-
-# # Compare historical and future time periods
-# if(location == "Nigeria" || t.param$Habitat == "Tropical") {
-#   plot.compare <- ggdraw()  +
-#     draw_plot(band.temp, x = 0, y = 0, width = 1, height = 0.3) +
-#     draw_plot(plot.temp, x = 0, y = 0, width = 1, height = 0.3) +
-#     draw_plot(plot.temp.f, x = 0, y = 0, width = 1, height = 0.3) +
-#     draw_plot(band.density, x = 0, y = 0.3, width = 1, height = 0.7) +
-#     #draw_plot(model.J, x = 0, y = 0.3, width = 1, height = 0.7) +
-#     #draw_plot(model.A, x = 0, y = 0.3, width = 1, height = 0.7) +
-#     draw_plot(model.census, x = 0, y = 0.3, width = 1, height = 0.7) +
-#     #draw_plot(model.J.f, x = 0, y = 0.3, width = 1, height = 0.7) +
-#     #draw_plot(model.A.f, x = 0, y = 0.3, width = 1, height = 0.7) +
-#     draw_plot(plot.census, x = 0, y = 0.3, width = 1, height = 0.7)
-# }
-# if(str_split(location, boundary("word"), simplify = T)[,1] == "China" || t.param$Habitat != "Tropical") { 
-#   plot.compare <- ggdraw()  +
-#     draw_plot(band.summer.temp, x = 0, y = 0, width = 1, height = 0.3) +
-#     draw_plot(plot.temp, x = 0, y = 0, width = 1, height = 0.3) +
-#     draw_plot(plot.temp.f, x = 0, y = 0, width = 1, height = 0.3) +
-#     draw_plot(band.summer.density, x = 0, y = 0.3, width = 1, height = 0.7) +
-#     #draw_plot(model.J, x = 0, y = 0.3, width = 1, height = 0.7) +
-#     draw_plot(model.A, x = 0, y = 0.3, width = 1, height = 0.7) +
-#     #draw_plot(model.census, x = 0, y = 0.3, width = 1, height = 0.7) +
-#     #draw_plot(model.J.f, x = 0, y = 0.3, width = 1, height = 0.7) +
-#     draw_plot(model.A.f, x = 0, y = 0.3, width = 1, height = 0.7) #+
-#     #draw_plot(plot.census, x = 0, y = 0.3, width = 1, height = 0.7)
-# }
-
-
-# # VIEW IN RSTUDIO
-# #plot
-# plot.compare
-# 
-# 
-# # STATISTICS
-# #linear regression of field data and model data
-# stats.data <- data.frame(Time = data.census$time, Census = data.census$A, Model = NA)
-# for(i in seq(1:nrow(stats.data))) {
-#   stats.data$Model[i] = model.r.census[model.r.census$Time == stats.data$Time[i],"A"]
-# }
-# summary(lm(stats.data$Model ~ 0 + stats.data$Census))
-# plot(stats.data$Census,stats.data$Model, xlim = c(0,100), ylim = c(0,100))
-
-
-# OUTPUT PLOTS
-#dev.new()
-# Temperature plots
-# if(location == "Nigeria" || t.param$Habitat == "Tropical") {
-#   ggdraw()  +
-#     draw_plot(band.temp, x = 0, y = 0, width = 1, height = 0.3) +
-#     draw_plot(plot.temp, x = 0, y = 0, width = 1, height = 0.3) +
-#     draw_plot(plot.temp.f, x = 0, y = 0, width = 1, height = 0.3) }
-# if(str_split(location, boundary("word"), simplify = T)[,1] == "China" || t.param$Habitat != "Tropical") {
-#   ggdraw()  +
-#     draw_plot(band.summer.temp, x = 0, y = 0, width = 1, height = 0.3) +
-#     draw_plot(plot.temp, x = 0, y = 0, width = 1, height = 0.3) +
-#     draw_plot(plot.temp.f, x = 0, y = 0, width = 1, height = 0.3) }
-
-# Climate change plots
-# Tropical species
-# if(location == "Nigeria" || t.param$Habitat == "Tropical") {
-# # juveniles
-#   ggdraw()  +
-#     draw_plot(band.density, width = 1, height = 0.4) +
-#     draw_plot(model.J, width = 1, height = 0.4) +
-#     draw_plot(model.J.f, width = 1, height = 0.4) }
-#if(location == "Nigeria" || t.param$Habitat == "Tropical") {
-## adults
-  # ggdraw()  +
-  #   draw_plot(band.density, width = 1, height = 0.4) +
-  #   draw_plot(model.A, width = 1, height = 0.4) +
-  #   #draw_plot(model.census, width = 1, height = 0.4) +
-  #   draw_plot(model.A.f, width = 1, height = 0.4) } #+
-  #   #draw_plot(plot.census,width = 1, height = 0.4) }
-
-# Temperate species
-# if(str_split(location, boundary("word"), simplify = T)[,1] == "China" || t.param$Habitat != "Tropical") {
-## juveniles
-#   ggdraw()  +
-#     draw_plot(band.summer.density, width = 1, height = 0.4) +
-#     draw_plot(model.J, width = 1, height = 0.4) +
-#     draw_plot(model.J.f, width = 1, height = 0.4) }
-# if(str_split(location, boundary("word"), simplify = T)[,1] == "China" || t.param$Habitat != "Tropical") {
-## adults
-#   ggdraw()  +
-#     draw_plot(band.summer.density, width = 1, height = 0.4) +
-#     draw_plot(model.A, width = 1, height = 0.4) +
-#     draw_plot(model.census, width = 1, height = 0.4) +
-#     draw_plot(model.A.f, width = 1, height = 0.4) +
-#     draw_plot(plot.census,width = 1, height = 0.4) }
+# Adult density plot (Fig. 1G,H)
+if(t.param$Habitat == "Tropical") {
+  ggdraw()  +
+    draw_plot(band.density, x = 0, y = 0, width = 1, height = 0.4) +
+    draw_plot(model.A.r, x = 0, y = 0, width = 1, height = 0.4) +
+    draw_plot(model.A.f, x = 0, y = 0, width = 1, height = 0.4)
+} else { 
+  ggdraw()  +
+    draw_plot(band.summer.density, x = 0, y = 0, width = 1, height = 0.4) +
+    draw_plot(model.A.r, x = 0, y = 0, width = 1, height = 0.4) +
+    draw_plot(model.A.f, x = 0, y = 0, width = 1, height = 0.4)
+}
